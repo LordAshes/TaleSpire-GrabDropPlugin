765ad861cc26c019928f98b1ff17af95dfa8bab1
Lord Ashes <webtable@gmail.com>
2021-06-28 08:42:38 -0400

    Add files via upload

---
+++ TaleSpireGrabDropPlugin/GrabDropPlugin.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using BepInEx;
+using Bounce.Unmanaged;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+using BepInEx.Configuration;
+
+namespace LordAshes
+{
+    [BepInPlugin(Guid, "Grab Drop Plug-In", Version)]
+    [BepInDependency(RadialUI.RadialUIPlugin.Guid)]
+    [BepInDependency(StatMessaging.Guid)]
+    [BepInDependency(FileAccessPlugin.Guid)]
+    public class GrabDropPlugin : BaseUnityPlugin
+    {
+        // Plugin info
+        public const string Guid = "org.lordashes.plugins.grabdrop";
+        public const string Version = "1.0.0.0";
+
+        // Content directory
+        private string dir = UnityEngine.Application.dataPath.Substring(0, UnityEngine.Application.dataPath.LastIndexOf("/")) + "/TaleSpire_CustomData/";
+
+        // Track radial asset
+        private CreatureGuid radialCreature = CreatureGuid.Empty;
+
+        private ConfigEntry<bool> actOnMini;
+
+        /// <summary>
+        /// Function for initializing plugin
+        /// This function is called once by TaleSpire
+        /// </summary>
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Lord Ashes Grab Drop Plugin Active.");
+
+            // Post plugfin on TS main page
+            StateDetection.Initialize(this.GetType());
+
+            // Get operation order
+            actOnMini = Config.Bind("Settings", "Select prop, action on mini", true);
+
+            // Create grab menu entry
+            RadialUI.RadialUIPlugin.AddOnCharacter(Guid+".grab", new MapMenu.ItemArgs
+            {
+                Action = (mmi, obj) =>
+                {
+                    if (actOnMini.Value)
+                    {
+                        StatMessaging.SetInfo(radialCreature, GrabDropPlugin.Guid, LocalClient.SelectedCreatureId.ToString());
+                    }
+                    else
+                    {
+                        StatMessaging.SetInfo(LocalClient.SelectedCreatureId, GrabDropPlugin.Guid, radialCreature.ToString());
+                    }
+                },
+                Icon = FileAccessPlugin.Image.LoadSprite("Grab.png"),
+                Title = "Grab",
+                CloseMenuOnActivate = true
+            }, Reporter);
+
+            // Create drop menu entry
+            RadialUI.RadialUIPlugin.AddOnCharacter(Guid+".drop", new MapMenu.ItemArgs
+            {
+                Action = (mmi, obj) =>
+                {
+                    StatMessaging.SetInfo(radialCreature, GrabDropPlugin.Guid, "");
+                },
+                Icon = FileAccessPlugin.Image.LoadSprite("Drop.png"),
+                Title = "Drop",
+                CloseMenuOnActivate = true
+            }, Reporter);
+
+            // Subscribe to grab and drop requests
+            StatMessaging.Subscribe(GrabDropPlugin.Guid, (changes) =>
+            {
+                foreach (StatMessaging.Change change in changes)
+                {
+                    if (change.value != "")
+                    {
+                        // Grab
+                        CreatureBoardAsset asset;
+                        CreaturePresenter.TryGetAsset(change.cid, out asset);
+                        if (asset != null)
+                        {
+                            CreatureBoardAsset child;
+                            CreaturePresenter.TryGetAsset(new CreatureGuid(change.value), out child);
+                            if (child != null)
+                            {
+                                Debug.Log(StatMessaging.GetCreatureName(asset.Creature) + " grabs '" + child.Creature.Name + "'");
+                                child.transform.SetParent(asset.transform);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Drop
+                        CreatureBoardAsset asset;
+                        CreaturePresenter.TryGetAsset(change.cid, out asset);
+                        foreach (Transform child in asset.transform.Children())
+                        {
+                            if ((child.gameObject.name.StartsWith("Custom:")) || (child.gameObject.name.Contains("(Clone)")))
+                            {
+                                Debug.Log(StatMessaging.GetCreatureName(asset.Creature) + " drops '" + child.gameObject.name + "'");
+                                child.transform.SetParent(null);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// Method to track which asset has the radial menu open
+        /// </summary>
+        /// <param name="selected"></param>
+        /// <param name="radialMenu"></param>
+        /// <returns></returns>
+        private bool Reporter(NGuid selected, NGuid radialMenu)
+        {
+            radialCreature = new CreatureGuid(radialMenu);
+            return true;
+        }
+
+        /// <summary>
+        /// Function for determining if view mode has been toggled and, if so, activating or deactivating Character View mode.
+        /// This function is called periodically by TaleSpire.
+        /// </summary>
+        void Update()
+        {
+        }
+    }
+}
---
+++ TaleSpireGrabDropPlugin/StateDetection.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LordAshes
+{
+    public static class StateDetection
+    {
+        public static void Initialize(System.Reflection.MemberInfo plugin)
+        {
+            SceneManager.sceneLoaded += (scene, mode) =>
+            {
+                try
+                {
+                    if (scene.name == "UI")
+                    {
+                        TextMeshProUGUI betaText = GetUITextByName("BETA");
+                        if (betaText)
+                        {
+                            betaText.text = "INJECTED BUILD - unstable mods";
+                        }
+                    }
+                    else
+                    {
+                        TextMeshProUGUI modListText = GetUITextByName("TextMeshPro Text");
+                        if (modListText)
+                        {
+                            BepInPlugin bepInPlugin = (BepInPlugin)Attribute.GetCustomAttribute(plugin, typeof(BepInPlugin));
+                            if (modListText.text.EndsWith("</size>"))
+                            {
+                                modListText.text += "\n\nMods Currently Installed:\n";
+                            }
+                            modListText.text += "\nLord Ashes' " + bepInPlugin.Name + " - " + bepInPlugin.Version;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.Log(ex);
+                }
+            };
+        }
+
+        public static TextMeshProUGUI GetUITextByName(string name)
+        {
+            TextMeshProUGUI[] texts = UnityEngine.Object.FindObjectsOfType<TextMeshProUGUI>();
+            for (int i = 0; i < texts.Length; i++)
+            {
+                if (texts[i].name == name)
+                {
+                    return texts[i];
+                }
+            }
+            return null;
+        }
+    }
+}
