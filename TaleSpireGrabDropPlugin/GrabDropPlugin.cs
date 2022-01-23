0d29005b535b29f0d996bb2777264bd84cd47c74
Lord Ashes <webtable@gmail.com>
2022-01-22 19:11:09 -0500

    Version 1.4.5

--- TaleSpireGrabDropPlugin/GrabDropPlugin.cs
+++ TaleSpireGrabDropPlugin/GrabDropPlugin.cs
@@ -15,7 +15,7 @@ namespace LordAshes
     {
         // Plugin info
         public const string Guid = "org.lordashes.plugins.grabdrop";
-        public const string Version = "1.4.0.0";
+        public const string Version = "1.4.5.0";
         public const string StatesPluginGuid = "org.lordashes.plugins.states";
 
         // Content directory
